01dd9d0988112b9f01fb1c98b7e0577db922f8e3
George Thomas <georgetamayothomas@gmail.com>
2016-12-10 21:48:47 +0000

    Add day three part two

--- 03/main.py
+++ 03/main.py
@@ -1,1 +1,16 @@
 print(len([x for x in [sorted([int(y) for y in line.split()]) for line in open('triangles.txt', 'r').readlines()] if x[0] + x[1] > x[2]]))
+
+f = open('triangles.txt', 'r')
+
+i = 0
+buf = [None]*3
+total = 0
+for line in f:
+    buf[i] = [int(y) for y in line.split()]
+    i = (i + 1) % 3
+    if i == 0:
+        for j in range(3):
+            x = sorted([x[j] for x in buf])
+            total += 1 if x[0] + x[1] > x[2] else 0
+print(total)
+
